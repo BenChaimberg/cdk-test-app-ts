a15b741c5a8dca650ebe7be911bf346d67206be5
Ben Chaimberg <chaimber@amazon.com>
2021-05-25 10:58:17 -0700

    init

---
+++ bin/test-app.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { TestAppStack } from '../lib/test-app-stack';
+
+const app = new cdk.App();
+new TestAppStack(app, 'TestAppStack');
