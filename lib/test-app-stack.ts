396bd77abb959a842c86c39dfc45fc59ed7e3977
Ben Chaimberg <chaimber@amazon.com>
2021-06-24 14:12:01 -0500

    remove initial resources

--- lib/test-app-stack.ts
+++ lib/test-app-stack.ts
@@ -1,18 +1,7 @@
-import { Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
-import { Queue } from '@aws-cdk/aws-sqs';
-import { SqsSubscription } from '@aws-cdk/aws-sns-subscriptions';
-import { Topic } from '@aws-cdk/aws-sns';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
 
 export class TestAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new Queue(this, 'Queue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new Topic(this, 'Topic');
-
-    topic.addSubscription(new SqsSubscription(queue));
   }
 }
